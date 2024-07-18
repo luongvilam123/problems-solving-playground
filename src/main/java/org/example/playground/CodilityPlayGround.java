054779cd9e6e07c6cfc94074de69d649460a60fa
lamluongit2000@gmail.com <kulam1782000@gmail.com>
2024-07-18 10:17:32 +0700

    add solutio for trapping water

--- src/main/java/org/example/playground/CodilityPlayGround.java
+++ src/main/java/org/example/playground/CodilityPlayGround.java
@@ -473,9 +473,23 @@ public class CodilityPlayGround {
     }
     //https://leetcode.com/problems/trapping-rain-water/
     public int trap(int[] height) {
+        int result = 0;
         int left = 0, right = height.length - 1;
+        int maxLeft = height[left];
+        int maxRight = height[right];
 
-        return 1;
+        while(left < right) {
+            if( maxLeft < maxRight) {
+                left++;
+                maxLeft = Math.max(maxLeft,height[left]);
+                result = result + ( maxLeft - height[left] );
+            } else {
+                right--;
+                maxRight = Math.max(maxRight,height[right]);
+                result += maxRight -height[right];
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
