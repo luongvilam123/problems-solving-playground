082d78a8f96d63493cf5aba1c8b83b1e410ef3a9
lamluongit2000@gmail.com <kulam1782000@gmail.com>
2024-10-12 21:16:31 +0700

    add binary search

--- src/main/java/org/example/playground/CodilityPlayGround.java
+++ src/main/java/org/example/playground/CodilityPlayGround.java
@@ -893,7 +893,23 @@ public class CodilityPlayGround {
         dfs(result,subset,index + 1,nums);
 
     }
+    // https://leetcode.com/problems/binary-search/description/
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while( left <= right){
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target) return mid;
 
+            else if(target > nums[mid]){
+                left = mid + 1;
+            }
+            else if( target < nums[mid]){
+                right = mid -1;
+            }
+        }
+        return -1;
+    }
 
     public static void main(String[] args) {
         waiter(List.of(new Integer[]{2,3,4,5,6,7}), 3).forEach(System.out::println);
